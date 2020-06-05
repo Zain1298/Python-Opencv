cbb5688ebb17caef7548584a1dd24710c04218ac
Syed Zain Rashid <55804149+Zain1298@users.noreply.github.com>
2020-06-05 16:32:00 +0500

    Shows the information about an image.
    This program gives the info about an image in a tuple form like this (height,width,layers).

--- third opencv/third.py
+++ third opencv/third.py
@@ -5,6 +5,7 @@ img = cv2.imread('sz.jpeg')
 cv2.imshow('Output Image',img)
 
 print(img.shape)
+#print image shape in tuple form (height,width,layersofRGB)
 
 print("Value of Height:",img.shape[0])
 print("Value of Width:",img.shape[1])
